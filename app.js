dd16986a44a512148d2c928c43dbd1f2fd7e8d28
MarqCervMartin <martin_mcervantes@hotmail.com>
2021-01-25 00:12:13 -0600

    feat: Authenticacion con google

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 const botones = document.querySelector('#botones');
 const nombreUsuario = document.querySelector('#nombreUsuario');
+const contenidoProtegido = document.querySelector('#contenidoProtegido');
 
-firebase.auth().onAuthStateChanged((user) =>{
+firebase.auth().onAuthStateChanged( user =>{
     if(user){
         console.log(user);
         botones.innerHTML = `
-            <button class="btn btn-outline-danger">Cerrar</button>
+            <button class="btn btn-outline-danger" id='btnCerrarSesion'>Cerrar</button>
+        `
+        nombreUsuario.innerHTML = user.displayName;
+        cerrarSesion();
+        contenidoProtegido.innerHTML = `
+            <p class="text-center lead mt-5">Bienvenido ${user.email}</p>
         `
     }else{
-        //console.log('No existe user');
+        console.log('No existe user');
         botones.innerHTML = /*html*/`
-            <button class="btn btn-outline-success mr-2">Acceder</button>
+            <button class="btn btn-outline-success mr-2" id='btnAcceder'>Acceder</button>
+        `
+        iniciarSesion();
+        nombreUsuario.innerHTML = 'Chat'
+        contenidoProtegido.innerHTML = `
+            <p class="text-center lead mt-5">Debes iniciar sesión</p>
         `
     }
 })
+
+const cerrarSesion = () => {
+    const btnCerrarSesion = document.querySelector('#btnCerrarSesion');
+    btnCerrarSesion.addEventListener('click', () =>{
+        firebase.auth().signOut();
+    })
+}
+
+const iniciarSesion = () =>{
+    const btnAcceder = document.querySelector('#btnAcceder');
+    btnAcceder.addEventListener('click', async() =>{
+        //console.log('Me diste click en acceder');
+        try{
+            const provider = new firebase.auth.GoogleAuthProvider();
+            await firebase.auth().signInWithPopup(provider)
+        }catch(error){
+            console.log(error)
+        }
+    })
+}
